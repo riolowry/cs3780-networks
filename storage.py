b6de3b2464d448d99ab232ce5c18719be64a60b2
Lezar <lezar@lezar.lezar>
2013-11-14 11:01:38 -0700

    Update storage to write and read to file per #2

--- storage.py
+++ storage.py
@@ -1,14 +1,33 @@
+import json
+
 class MessageStorage():
 
     def __init__(self):
-        self.messages = {}
+        self.messages = self.read_from_file()
 
     def add_message(self, message):
         # Add a message with its destination as key
         destination = message["Destination"]
         if destination not in self.messages:
             self.messages[destination] = []
-        else:
-            self.messages[destination].append(message)
+        self.messages[destination].append(message)
 
-        print self.messages[destination]
+        self.write_to_file()
+
+    def write_to_file(self):
+        # Write messages to file
+        try:
+            with open('savedmessages.json','w') as f:
+                json.dump(self.messages, f)
+        except IOError, e:
+            print "I/O error: %s" % e
+
+    def read_from_file(self):
+        # Read messages from file
+        try:
+            with open('savedmessages.json','r') as f:
+                return json.load(f)
+        except IOError, e:
+            print "I/O error: %s" % e
+            return {}
+ 
